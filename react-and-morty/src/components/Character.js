8659c3e3f469debd1ac0a2d9d20d3065b072697e
IstvanPongracz <pongracz88@gmail.com>
2022-01-26 23:29:22 +0100

    Portal háttér

--- react-and-morty/src/components/Character.js
+++ react-and-morty/src/components/Character.js
@@ -9,30 +9,32 @@ const Character = (props) => {
   // bug (??) egyszerre tobb kartyat is meg lehet nyitni egymas tetejere
   return (
     <div className={isShown ? "detailedCards" : "miniCards"} onClick={showCharacterDetails}>
-      <div className="charName">
-        <p>{props.name}</p>
-      </div>
-      <div className="charPicture">
-        <img className="img" src={props.image} alt="" />
-      </div>
-      <div className="charSpecies">
-        <p>{props.species}</p>
-      </div>
-      {isShown && (
-        <div>
-          <p>Status: {props.status}</p>
+      <div className={isShown ? "portalCards" : ""}>
+        <div className="charName">
+          <p>{props.name}</p>
         </div>
-      )}
-      {isShown && (
-        <div>
-          <p>Gender: {props.gender}</p>
+        <div className="charPicture">
+          <img className="img" src={props.image} alt="" />
         </div>
-      )}
-      {isShown && (
-        <div>
-          <p>Location: {props.location}</p>
+        <div className="charSpecies">
+          <p>{props.species}</p>
         </div>
-      )}
+        {isShown && (
+          <div>
+            <p>Status: {props.status}</p>
+          </div>
+        )}
+        {isShown && (
+          <div>
+            <p>Gender: {props.gender}</p>
+          </div>
+        )}
+        {isShown && (
+          <div>
+            <p>Location: {props.location}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- react-and-morty/src/components/Location.js
+++ react-and-morty/src/components/Location.js
@@ -12,17 +12,19 @@ const Location = (props) => {
             className={isShown ? "locVisible" : "locCard"}
             onClick={showLocationDetails}
         >
-            <div className="locName">
-                <h2>{props.name}</h2>
-            </div>
-            <div className="locType">
-                <p>{props.type}</p>
-            </div>
-            {isShown && (
-                <div>
-                    <p>Dimension: {props.dimension}</p>
+            <div className={isShown ? "portalCardsLoc" : ""}>
+                <div className="locName">
+                    <h2>{props.name}</h2>
                 </div>
-            )}
+                <div className="locType">
+                    <p>{props.type}</p>
+                </div>
+                {isShown && (
+                    <div>
+                        <p>Dimension: {props.dimension}</p>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
