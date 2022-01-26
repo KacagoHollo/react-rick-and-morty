82c9b17d877fd2745648c256f63d483dd0a27835
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-01-26 15:27:48 +0100

    Add pagination

--- react-and-morty/src/components/Characters.js
+++ react-and-morty/src/components/Characters.js
@@ -1,33 +1,48 @@
 import React, { useState } from "react";
 import { useCharacters } from "../api/useData";
 import Character from "./Character";
+import Pagination from "@material-ui/lab/Pagination";
 
-const Characters = (props) => {
-    const [page, setPage] = useState(1);
-    const character = useCharacters(page);
+const Characters = () => {
+  const [page, setPage] = useState(1);
+  const character = useCharacters(page);
 
-    let characterList = [];
+  let characterList = [];
 
-    if (character !== "Loading..." && character !== undefined) {
-        characterList = character.results.map((item) => (
-            <Character
-                key={item.id}
-                image={item.image}
-                name={item.name}
-                species={item.species}
-                gender={item.gender}
-                origin={item.origin.name}
-                location={item.location.name}
-            />
-        ));
-    }
+  if (character !== "Loading..." && character !== undefined) {
+    characterList = character.results.map((item) => (
+      <Character
+        key={item.id}
+        image={item.image}
+        name={item.name}
+        species={item.species}
+        gender={item.gender}
+        origin={item.origin.name}
+        location={item.location.name}
+      />
+    ));
+  }
 
-    return (
-        <div className="character-list">
-            {characterList};
-            {/* //plusz itt neki volt pagination is */}
-        </div>
-    )
-}
+  const paginationEvent = (event, value) => {
+    setPage(value);
+  };
+
+  return (
+    <div className="character-list">
+      {characterList}
+      <div className="pagination">
+        <Pagination
+          count="42"
+          page={page}
+          onChange={paginationEvent}
+          defaultPage={1}
+          boundaryCount={2}
+          size="large"
+          color="secondary"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Characters;
--- react-and-morty/src/components/Locations.js
+++ react-and-morty/src/components/Locations.js
@@ -1,32 +1,40 @@
 import React, { useState } from "react";
 import Location from "./Location";
 import { useLocations } from "../api/useData";
+import Pagination from "@material-ui/lab/Pagination";
 
 const Locations = (props) => {
-    const [page, setPage] = useState(1);
-    const location = useLocations(page);
+  const [page, setPage] = useState(1);
+  const location = useLocations(page);
 
-    let locationList = [];
+  let locationList = [];
 
-    if (location !== "Loading..." && location !== undefined) {
-        locationList = location.results.map((item) => (
-            <Location
-                key={item.id}
-                name={item.name}
-                type={item.type}
-                dimension={item.dimension}
-                created={item.created}
-            />
-        ));
-    }
+  if (location !== "Loading..." && location !== undefined) {
+    locationList = location.results.map((item) => (
+      <Location key={item.id} name={item.name} type={item.type} dimension={item.dimension} created={item.created} />
+    ));
+  }
 
-    return (
-        <div>
-            <div className="location-list">
-                {locationList}
-            </div>;
-        </div>
-    );
-}
+  const paginationEvent = (event, value) => {
+    setPage(value);
+  };
+
+  return (
+    <div>
+      <div className="location-list">{locationList}</div>;
+      <div className="pagination">
+        <Pagination
+          count="7"
+          page={page}
+          onChange={paginationEvent}
+          defaultPage={1}
+          boundaryCount={2}
+          size="large"
+          color="secondary"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Locations;
