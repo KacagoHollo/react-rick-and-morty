7b3f110da3f9147141045d4c8816101cfba07014
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-01-27 13:02:18 +0100

    Undo changes to test, nothing works

--- react-and-morty/src/components/Description.js
+++ react-and-morty/src/components/Description.js
@@ -1,17 +1,21 @@
 import React from "react";
 
 const Description = () => {
-    return (
-        <div className="description">
-            <p>
-                Rick and Morty is an American adult animated science fiction sitcom created by Justin Roiland and Dan Harmon for Cartoon Network's nighttime programming block, Adult Swim. The series follows the misadventures of cynical mad scientist Rick Sanchez and his good-hearted, but fretful grandson Morty Smith, who split their time between domestic life and interdimensional adventures.
-                <br />
-                The Rick and Morty universe is kinda big with lot of characters and locations. We would like to make it easier to look up information about each characters and locations.
-                <br />
-                Click on any of the buttons to see the related data. On the cards click again to toggle further informations.
-            </p>
-            <h1>Wubba Lubba dub dub!!!</h1>
-        </div>
-    );
+  return (
+    <div className="description">
+      <p>
+        Rick and Morty is an American adult animated science fiction sitcom created by Justin Roiland and Dan Harmon for
+        Cartoon Network's nighttime programming block, Adult Swim. The series follows the misadventures of cynical mad
+        scientist Rick Sanchez and his good-hearted, but fretful grandson Morty Smith, who split their time between
+        domestic life and interdimensional adventures.
+        <br />
+        The Rick and Morty universe is kinda big with lot of characters and locations. We would like to make it easier
+        to look up information about each characters and locations.
+        <br />
+        Click on any of the buttons to see the related data. On the cards click again to toggle further informations.
+      </p>
+      <h1>Wubba Lubba dub dub!!!</h1>
+    </div>
+  );
 };
 export default Description;
