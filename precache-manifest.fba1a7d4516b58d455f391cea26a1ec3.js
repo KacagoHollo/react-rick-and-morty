dc33912e332fac6171fc162b82b78377734dddeb
IstvanPongracz <pongracz88@gmail.com>
2022-03-18 12:36:50 +0100

    Updates

---
+++ precache-manifest.fba1a7d4516b58d455f391cea26a1ec3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba3d9c434421f0080734d2f3e63e525",
+    "url": "/react-rick-and-morty/index.html"
+  },
+  {
+    "revision": "6c56d10be3e10d7b5699",
+    "url": "/react-rick-and-morty/static/css/main.691608e3.chunk.css"
+  },
+  {
+    "revision": "73bf9c534973abd1b7a3",
+    "url": "/react-rick-and-morty/static/js/2.af1fe589.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-rick-and-morty/static/js/2.af1fe589.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c56d10be3e10d7b5699",
+    "url": "/react-rick-and-morty/static/js/main.fce06255.chunk.js"
+  },
+  {
+    "revision": "4faca91fa76480e14626",
+    "url": "/react-rick-and-morty/static/js/runtime-main.d376f658.js"
+  },
+  {
+    "revision": "4ca43f48c936b70fb2ab7ae8f47b8d9d",
+    "url": "/react-rick-and-morty/static/media/Rick_and_Morty_Logo_and_Image.4ca43f48.png"
+  },
+  {
+    "revision": "acff25a190649fc23b3ad1579f0cad56",
+    "url": "/react-rick-and-morty/static/media/bckgrnd_rm.acff25a1.jpg"
+  },
+  {
+    "revision": "4b4420cffe3ba4f13221e529e5b60078",
+    "url": "/react-rick-and-morty/static/media/get_schwifty.4b4420cf.woff"
+  },
+  {
+    "revision": "24278ae24439d70ae07bce843c144288",
+    "url": "/react-rick-and-morty/static/media/portal1.24278ae2.png"
+  }
+]);
